1e4a33229d6c8a93584741787987b2e58f89ed90
Oleg.Galtsov <Oleg.Galtsov@mvideo.ru>
2021-04-15 18:29:40 +0300

    install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl https://github.com/ogaltsov/ansible-encrypt-vault/blob/master/ansible-encrypt-vault.py | bash
+curl -sL https://raw.githubusercontent.com/ogaltsov/ansible-encrypt-vault/master/ansible-encrypt-vault.py | bash
 chmod +x ansible-encrypt-vault.py
 mv ansible-encrypt-vault.py /usr/local/bin/ansible-encrypt-vault
 
